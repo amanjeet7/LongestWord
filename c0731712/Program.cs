d5f4bb59030262d906518e4f756f9cfd9417242c
Amanjeetsingh Bajwa <c0731712@mylambton.ca>
2019-04-18 15:19:49 -0400

    starting of longest word and starter code

--- c0731712/Program.cs
+++ c0731712/Program.cs
@@ -8,8 +8,38 @@ namespace c0731712
 {
     class Program
     {
+    
         static void Main(string[] args)
         {
+            Program a = new Program();
+            Console.WriteLine("The longest word is: ");
+            a.MyFunctionA("Ada Lovelace wrote the first algorithm designed for procession by an Analytical Engine");
+        }
+
+        public MyFunctionA(string iutput)
+        {
+            //write a c#program to find the longest word in a string.
+            string[] words = iutput.Split(' ');
+            
+            
+            int wordArrayLength = words.Length;
+            int[]wordsLength = new int[wordArrayLength];
+            int x = 0;
+            foreach(var word in words)
+            {
+                
+                foreach(var word2 in words)
+                {
+                    if(word2.Length > word.Length)
+                    {
+                        a = word2;
+                        
+                    }
+                }
+            }
+            Console.WriteLine("Longest word in string is:  " + a);
+            Console.ReadLine();
+            
         }
     }
 }
